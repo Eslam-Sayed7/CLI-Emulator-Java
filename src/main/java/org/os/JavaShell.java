262438fa5ab060ec7fef40083e166a10087d8b7b
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-10-30 13:06:03 +0300

    add cat, ls-a and ls-r

--- src/main/java/org/os/JavaShell.java
+++ src/main/java/org/os/JavaShell.java
@@ -246,6 +246,31 @@ public class JavaShell {
         }
     }
 
+    private File[] LS(boolean showAll, boolean reverseOrder) {
+        File[] contents = currentDirectory.listFiles();
+
+        if (contents == null) {
+            System.out.println("Directory is inaccessible");
+            return new File[0];
+        }
+
+        // Filter out hidden files if showAll is false
+        if (!showAll) {
+            contents = Arrays.stream(contents)
+                    .filter(file -> !file.isHidden())
+                    .toArray(File[]::new);
+        }
+
+        // Reverse order if reverseOrder is true
+        if (reverseOrder) {
+            Arrays.sort(contents, (f1, f2) -> f2.getName().compareTo(f1.getName()));
+        } else {
+            Arrays.sort(contents, (f1, f2) -> f1.getName().compareTo(f2.getName()));
+        }
+
+        return contents;
+    }
+
     public void runShell() {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String command;
@@ -289,7 +314,10 @@ public class JavaShell {
                     case "help" -> help();
                     case "mkdir"->mkdirCommand(binarytokens);
                     case "cat" -> Cat(binarytokens[1]);
+                    case "ls-a" -> STDprintFunctionOutput(() -> LS(true, false));
+                    case "ls-r" -> STDprintFunctionOutput(() -> LS(false, true));
                 }
+
 
             } else { // more than two token [ | - >> - > ] tools
                 String[] tokens = command.split("\\s+");
